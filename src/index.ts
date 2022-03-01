4a4995157b98ac72dd602be468184ddc74b76f4d
minidonut <nycom13@gmail.com>
2022-03-01 21:56:34 +0900

    feat: impl main function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello ts-app");
+import { fetchSlackStatusPage } from './http';
+import { parseSlackStatus } from './parser';
+import { SlackStatus } from './types';
+
+export * from './error';
+export * from './types';
+export { SlackStatusType } from './constant';
+
+function createSlackStatusFetcher(fetchHtml: () => Promise<string>, parse: (html: string) => SlackStatus) {
+  return () => fetchHtml().then(parse);
+}
+
+const fetchSlackStatus = createSlackStatusFetcher(fetchSlackStatusPage, parseSlackStatus);
+
+export { fetchSlackStatus };
+export default fetchSlackStatus;
